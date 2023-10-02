e443540aebb736d37cbb3892b935358dd2da2415
Túlio <lucastuliolima@gmail.com>
2023-10-02 16:44:58 -0300

    Solução do desafio

--- src/Contador.java
+++ src/Contador.java
@@ -8,18 +8,30 @@ public class Contador {
         System.out.println("Insira o segundo parâmetro: ");
         int parametroDois = terminal.nextInt();
 
+        terminal.close();
         try {
             //lógica contagem
             contar(parametroUm, parametroDois);            
-        } catch( err) {
+        } catch(ParametrosInvalidosException err) {
+            System.err.println("O segundo parametro deve ser maior do que o primeiro.");
+
             //print msg: o parâmetro 2 deve ser maior que o primeiro
         }
 
     }
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-        //validar se parametroUm é maior que dois e lançar a exceção
-        
         int contagem = parametroDois - parametroUm;
+        //validar se parametroUm é maior que dois e lançar a exceção
+        if (contagem < 0) {
+            throw new ParametrosInvalidosException();
+        } else {
+            for (int i = parametroUm; i <= parametroDois; i++) {
+            System.out.println(i);
+            }
+        }
+        
         //realizar o for para imprimir os números com base na variável contagem;
+        
+
     }
 }
